ab8d393a107e7d058e109096c5756ed7321294d9
lordaake <lordaake@protonmail.com>
2023-10-15 16:20:02 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -8,41 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     menuItems.forEach((item) => {
-        item.addEventListener("click", function (e) {
-            e.preventDefault();
+        item.addEventListener("click", function () {
             menuContainer.classList.remove("menu-open");
-
-            const targetId = item.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
-
-            if (targetSection) {
-                window.scrollTo({
-                    top: targetSection.offsetTop,
-                    behavior: "smooth",
-                });
-            }
         });
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    let menuLinks = document.querySelectorAll('.menu-list a');
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-
-            let targetId = this.getAttribute('href');
-            let targetSection = document.querySelector(targetId);
-
-            targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        });
-    });
-});
 
 let backToTopButton = document.getElementById("backToTop");
 
 backToTopButton.addEventListener("click", function () {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollIntoView({ behavior: "smooth" });
 });
